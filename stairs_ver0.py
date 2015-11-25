f65b3c0b571965653f91f95ae3e59ee230fc9542
martind <martind@mapfactor.com>
2015-11-25 22:08:27 +0100

    first motion (slowly forward and backward, jump fails)

---
+++ stairs_ver0.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+"""
+  Climbing stairs (demo for Tour-the-Stairs 2015)
+  usage:
+       ./stairs_ver0.py <num stepss>
+"""
+import sys
+
+import sumo # from https://github.com/haraisao/JumpingSumo-Python.git 
+import time
+
+def demo( numSteps ):
+    cnt = sumo.SumoController('MyCtrl')
+    cnt.connect()
+    cnt.move(10)
+    time.sleep(3.0)
+    cnt.move(-10)
+    time.sleep(3.0)
+    cnt.move(0)
+    cnt.jump(1) # somehow does not work
+    time.sleep(3.0)
+    cnt.move(0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print __doc__
+        sys.exit(2)
+    demo( int(sys.argv[1]) )
+
+# vim: expandtab sw=4 ts=4 
+
